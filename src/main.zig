b8e966c0c84d850d21702ab45ad64de26227e657
Harry Yu <yupakhei456@gmail.com>
2025-10-01 18:00:27 -0400

    Send http1.1 200 response

--- src/main.zig
+++ src/main.zig
@@ -2,12 +2,19 @@ const std = @import("std");
 const net = std.net;
 
 pub fn main() !void {
-    std.debug.print("----- Zig TCP/IP server -----", .{});
+    std.debug.print("----- Zig TCP/IP server -----\n", .{});
 
     const address = try net.Address.resolveIp("127.0.0.1", 4221);
-    var listener = try address.listen(.{ .reuse_address = true });
-    defer listener.deinit();
+    var server = try address.listen(.{ .reuse_address = true });
+    defer server.deinit();
+    const connection = try server.accept();
 
-    _ = try listener.accept();
+    var buf: [1024]u8 = undefined;
+    var writer = connection.stream.writer(&buf);
+    const stream = &writer.interface;
+
+    _ = try stream.write("HTTP/1.1 200 OK\r\n\r\n");
+    _ = try stream.flush();
+
     std.debug.print("Client connected", .{});
 }
